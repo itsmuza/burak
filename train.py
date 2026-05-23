8b01d5ec47d908852ef6188a50ad44f04c6a134c
Muzaffar <itsmuuza@icloud.com>
2026-05-24 02:09:33 +0900

    task m in train.py complete

--- train.py
+++ train.py
@@ -1,9 +1,21 @@
-def reverseSentence(data):
-    arr = data.split(" ")
-    new_arr = []
-    for word in arr:
-        new_arr.append(word[::-1])
-    return " ".join(new_arr)
+# task - M
+def getSquareNumbers(data):
+    result = []
+    for n in data:
+        result.append({"number": n, "square": n**2})
+    return result
 
 
-print(reverseSentence("we like coding!"))
+print(getSquareNumbers([1, 2, 3]))
+
+
+# task - L
+# def reverseSentence(data):
+#     arr = data.split(" ")
+#     new_arr = []
+#     for word in arr:
+#         new_arr.append(word[::-1])
+#     return " ".join(new_arr)
+
+
+# print(reverseSentence("we like coding!"))

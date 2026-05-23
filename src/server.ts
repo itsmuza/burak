24a535842f8d11bc1f5d5c014bd39fe0f6313b63
Muzaffar <itsmuuza@icloud.com>
2026-05-24 01:35:18 +0900

    connect mongodb via mongoose

--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,11 @@
 // const person: string = "muza"
 // const number: number = 200
 
-import dotenv from "dotenv"
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 dotenv.config();
 
-console.log("PORT:", process.env.PORT)
-
-console.log("MONGO_URL:", process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL as string, {}).then(data => {
+    console.log("MONGODB connection succed");
+    const PORT = process.env.PORT ?? 3003
+}).catch(err => console.log("ERROR to connect MONGODB", err));

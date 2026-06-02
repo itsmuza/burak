44efb24a0587a2cafcf69c5fc651700a5f61e754
Muzaffar <itsmuuza@icloud.com>
2026-06-02 15:20:58 +0900

    develop singup process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,6 +1,29 @@
-class MemberService {
-    constructor() {
+import { MemberType } from "../libs/enums/member.enum";
+import Errors, { HttpCode, Message } from "../libs/Errors";
+import { Member, MemberInput } from "../libs/types/member";
+import MemberModel from "../schema/Member.model";
 
+class MemberService {
+    private readonly memberModel;
+
+    constructor() {
+        this.memberModel = MemberModel;
+    }
+
+    public async processSignup(input: MemberInput): Promise<Member> {
+        const exist = await this.memberModel
+            .findOne({memberType: MemberType.RESTAURANT})
+            .exec()
+        console.log("exist:", exist)
+        if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+        try {
+            const result = await this.memberModel.create(input);
+            result.memberPassword = "";
+            console.log("passed here");
+            return result;
+        } catch (err) {
+            throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+        }
     }
 }
 

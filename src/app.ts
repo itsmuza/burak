cb53fbc71bae00f3e80f657907cdaa8456b94225
Muzaffar <itsmuuza@icloud.com>
2026-06-08 10:41:40 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,15 @@ import routerAdmin from "./routerAdmin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
+});
+
 // 1 - Entrance
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
@@ -13,6 +22,17 @@ app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
 // 2 - sessions
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 3, // hour
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
 
 // 3 - views
 app.set("views", path.join(__dirname, "views"));

cd22034bcb8a93002e7188d6cc9924d3af8a9016
Muzaffar <itsmuuza@icloud.com>
2026-06-13 01:06:05 +0900

    fix: modify updateChosenProduct business login

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -49,7 +49,9 @@ productController.createNewProduct = async (
 
 productController.updateChosenProduct = async (req: Request, res: Response) => {
   try {
-    res.send("updateChosenProduct");
+    const id = req.params.id as string;
+    const result = await productService.updateChosenProduct(id, req.body);
+    res.status(HttpCode.OK).json({ data: result });
   } catch (err) {
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);

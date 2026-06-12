021d24e5d0f22fd3fea8b3bd05e05fdd4a0f4d21
Muzaffar <itsmuuza@icloud.com>
2026-06-13 00:26:40 +0900

    feat: createNewProduct business logic by restaurant member

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,13 +1,19 @@
 import { Request, Response } from "express";
 
 import { T } from "../libs/types/common";
-import Errors from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 import ProductService from "../models/Product.service";
 import { AdminRequest } from "../libs/types/member";
+import { ProductInput } from "../libs/types/product";
+import ProductModel from "../schema/Product.model";
 
 const productService = new ProductService();
 
 const productController: T = {};
+
+//* SPA
+
+//* SSR
 productController.getAllProduct = async (req: AdminRequest, res: Response) => {
   try {
     console.log("getAllProduct");
@@ -18,12 +24,26 @@ productController.getAllProduct = async (req: AdminRequest, res: Response) => {
   }
 };
 
-productController.createNewProduct = async (req: Request, res: Response) => {
+productController.createNewProduct = async (
+  req: AdminRequest,
+  res: Response,
+) => {
   try {
-    res.send("createNewProduct");
+    if (!req.files?.length)
+      throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
+
+    const data: ProductInput = req.body;
+    data.productImages = req.files?.map((ele) => ele.path.replace(/\\/g, "/"));
+    await productService.createNewProduct(data);
+    res.send(
+      `<script> alert("Sucessful creation!"); window.location.replace('admin/product/all')</script>`,
+    );
   } catch (err) {
-    if (err instanceof Errors) res.status(err.code).json(err);
-    else res.status(Errors.standard.code).json(Errors.standard);
+    const message =
+      err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+    res.send(
+      `<script> alert("${message}"); window.location.replace('admin/product/all')</script>`,
+    );
   }
 };
 

993e90fb6ad20972bd59a6601749c81555604cec
Muzaffar <itsmuuza@icloud.com>
2026-06-19 18:13:01 +0900

    feat: complete admin signup page

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,28 @@
 console.log("Signup frontend javascript file");
 
-$(function () {});
+$(function () {
+  const fileTarget = $(".file-box .upload-hidden");
+  let filename;
+
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = this.files[0];
+      const fileType = uploadFile["type"];
+      const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+      if (!validImageType.includes(fileType)) {
+        alert("Please insert only jpeg, jpg and png!");
+      } else {
+        if (uploadFile) {
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+        }
+        filename = this.files[0].name;
+      }
+      $(this).siblings(".upload-name").val(filename);
+    }
+  });
+});
 
 function validateSignupForm() {
   const memberNick = $(".member-nick").val();
@@ -21,5 +43,12 @@ function validateSignupForm() {
     alert("Password differs, please check!");
     return false;
   }
-  return true;
+
+  const memberImage = $(".member-image").get(0).files[0].name
+    ? $(".member-image").get(0).files[0].name
+    : null;
+  if (!memberImage) {
+    alert("Please insert restaurant image!");
+    return false;
+  }
 }

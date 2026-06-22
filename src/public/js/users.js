0db8a3478a833c19c1df45ad049baff5dc074719
Muzaffar <itsmuuza@icloud.com>
2026-06-23 00:40:06 +0900

    feat: frontend chosen user update logic develop

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,1 +1,22 @@
 console.log("Users frontend javascript file");
+
+$(function () {
+  $(".member-status").on("change", function (e) {
+    const id = this.id;
+    const memberStatus = $(this).val();
+    axios
+      .post("/admin/user/edit", { _id: id, memberStatus: memberStatus })
+      .then((response) => {
+        const result = response.data;
+
+        if (result.data) {
+          console.log("User updated!");
+          $(".member-status").blur();
+        } else alert("User update failed!");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("User update failed!");
+      });
+  });
+});

9c82cb656ca8e72f0f872e2bd209e952124cc529
Muzaffar <itsmuuza@icloud.com>
2026-06-22 16:22:47 +0900

    feat: create new product frontend logic develop

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -30,6 +30,7 @@ productController.createNewProduct = async (
   res: Response,
 ) => {
   try {
+    console.log("req.body:", req.body);
     if (!req.files?.length)
       throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
 
@@ -37,13 +38,13 @@ productController.createNewProduct = async (
     data.productImages = req.files?.map((ele) => ele.path.replace(/\\/g, "/"));
     await productService.createNewProduct(data);
     res.send(
-      `<script> alert("Sucessful creation!"); window.location.replace('admin/product/all')</script>`,
+      `<script> alert("Sucessful creation!"); window.location.replace('/admin/product/all')</script>`,
     );
   } catch (err) {
     const message =
       err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
     res.send(
-      `<script> alert("${message}"); window.location.replace('admin/product/all')</script>`,
+      `<script> alert("${message}"); window.location.replace('/admin/product/all')</script>`,
     );
   }
 };

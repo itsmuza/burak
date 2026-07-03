64ca9ed4cc4650d1837671543b730b8e6a796606
Muzaffar <itsmuuza@icloud.com>
2026-07-04 00:35:17 +0900

    feat: create middleware methods and develop logout process

--- src/libs/types/member.ts
+++ src/libs/types/member.ts
@@ -1,6 +1,6 @@
 import { ObjectId } from "mongoose";
 import { Session } from "express-session";
-import { Request } from "express";
+import { request, Request } from "express";
 import MemberService from "../../models/Member.service";
 import { MemberStatus, MemberType } from "../enums/member.enum";
 
@@ -52,6 +52,12 @@ export interface MemberUpdateInput {
 export interface AdminRequest extends Request {
   member: Member;
   session: Session & { member: Member };
+  file: Express.Multer.File;
+  files: Express.Multer.File[];
+}
+
+export interface ExtendedRequest extends Request {
+  member: Member;
   file: Express.Multer.File;
   files: Express.Multer.File[];
 }

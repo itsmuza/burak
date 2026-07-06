6d42274653d9b6a5073ef44e22de22e93376066d
Muzaffar <itsmuuza@icloud.com>
2026-07-06 18:35:09 +0900

    feat: develop getTopUsers REST API

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -103,6 +103,19 @@ memberController.updateMember = async (req: ExtendedRequest, res: Response) => {
   }
 };
 
+memberController.getTopUsers = async (req: Request, res: Response) => {
+  try {
+    console.log("getTopUsers");
+    const result = await memberService.getTopUsers();
+    res.status(HttpCode.OK).json(result);
+  } catch (err) {
+    console.log("Error, getTopUsers");
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
+//* middleware
 memberController.verifyAuth = async (
   req: ExtendedRequest,
   res: Response,
